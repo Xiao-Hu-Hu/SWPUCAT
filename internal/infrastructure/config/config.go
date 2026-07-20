0ebf42eddc54c4b0f1c3ade0738a9be6e718e12d
Xiao-Hu-Hu <3624435880@qq.com>
2026-07-20 22:40:06 +0800

    fix(config): add env key replacer for proper environment variable mapping

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/spf13/viper"
@@ -64,6 +65,7 @@ func Load() (*Config, error) {
 
 	viper.AutomaticEnv()
 	viper.SetEnvPrefix("CSA")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
 	viper.SetDefault("server.port", 8080)
 	viper.SetDefault("server.mode", "debug")
