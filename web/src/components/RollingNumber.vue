24f3b54bcb926ae0250cfb808a527a24019fac81
Xiao-Hu-Hu <3624435880@qq.com>
2026-07-22 19:23:00 +0800

    fix: RollingNumber 高度塌陷，新文本改用正常流布局

--- web/src/components/RollingNumber.vue
+++ web/src/components/RollingNumber.vue
@@ -11,10 +11,10 @@ const props = withDefaults(defineProps<{
 const displayText = ref(props.text)
 const oldText = ref('')
 const animating = ref(false)
-const newTextTop = ref('0')
-const oldTextTop = ref('0')
-const oldTextOpacity = ref(1)
-const transitionEnabled = ref(false)
+const newTransform = ref('translateY(0)')
+const oldTransform = ref('translateY(0)')
+const oldOpacity = ref(1)
+const useTransition = ref(false)
 
 watch(() => props.text, async (newVal) => {
   if (newVal === displayText.value || !newVal) return
@@ -22,24 +22,25 @@ watch(() => props.text, async (newVal) => {
   oldText.value = displayText.value
   displayText.value = newVal
   animating.value = true
-  transitionEnabled.value = false
+  useTransition.value = false
 
-  newTextTop.value = '100%'
-  oldTextTop.value = '0'
-  oldTextOpacity.value = 1
+  // Position new text below, old text at origin
+  newTransform.value = 'translateY(100%)'
+  oldTransform.value = 'translateY(0)'
+  oldOpacity.value = 1
 
   await nextTick()
   requestAnimationFrame(() => {
-    transitionEnabled.value = true
-    newTextTop.value = '0'
-    oldTextTop.value = '-100%'
-    oldTextOpacity.value = '0'
+    useTransition.value = true
+    newTransform.value = 'translateY(0)'
+    oldTransform.value = 'translateY(-100%)'
+    oldOpacity.value = '0'
   })
 
   setTimeout(() => {
     animating.value = false
     oldText.value = ''
-    transitionEnabled.value = false
+    useTransition.value = false
   }, props.duration)
 })
 </script>
@@ -50,16 +51,16 @@ watch(() => props.text, async (newVal) => {
       v-if="animating"
       class="rolling-old"
       :style="{
-        opacity: oldTextOpacity,
-        top: oldTextTop,
-        transition: transitionEnabled ? `all ${duration}ms cubic-bezier(0.4, 0, 0.2, 1)` : 'none'
+        opacity: oldOpacity,
+        transform: oldTransform,
+        transition: useTransition ? `transform ${duration}ms cubic-bezier(0.4, 0, 0.2, 1), opacity ${duration}ms cubic-bezier(0.4, 0, 0.2, 1)` : 'none'
       }"
     >{{ oldText }}</span>
     <span
       class="rolling-new"
       :style="{
-        top: newTextTop,
-        transition: transitionEnabled ? `top ${duration}ms cubic-bezier(0.4, 0, 0.2, 1)` : 'none'
+        transform: newTransform,
+        transition: useTransition ? `transform ${duration}ms cubic-bezier(0.4, 0, 0.2, 1)` : 'none'
       }"
     >{{ displayText }}</span>
   </span>
@@ -71,14 +72,16 @@ watch(() => props.text, async (newVal) => {
   display: inline-block;
   overflow: hidden;
   vertical-align: baseline;
-  height: 1em;
-  line-height: 1;
 }
 
-.rolling-new,
+.rolling-new {
+  display: block;
+}
+
 .rolling-old {
   position: absolute;
   left: 0;
+  top: 0;
   width: 100%;
   white-space: nowrap;
 }
